36c1204f5b6a9f513b4ed7be6ff0d4ee62bc826d
Pro92 <weezy192@mail.ru>
2020-09-12 13:24:22 +0300

    #*(@$&*

--- app/js/main.js
+++ app/js/main.js
@@ -7,12 +7,13 @@ $(function () {
 
 
   $(".rate-star").rateYo({
-    rating: 4.5,
+    rating: '4.5',
     readOnly: true,
     starWidth: "14px",
-    ratedFill: "#ffc000",
-    normalFill: "#ffffff",
-    spacing: "3px"
+    normalFill: '#cccccc',
+    ratedFill: '#ffc000',
+    spacing: "3px",
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>'
   });
 
 });
