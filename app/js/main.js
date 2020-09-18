f858036f67a31b6d2f4c2c456daddb0e428173f1
Pro92 <weezy192@mail.ru>
2020-09-18 17:44:52 +0300

    added feedback and news sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-const autoPrefixer = require("gulp-autoprefixer");
 
 $(function () {
   $('.slider-products').slick({
@@ -28,15 +27,20 @@ $(function () {
     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>'
   });
 
- 
+  let mySwiper = new Swiper('.feedback__swiper-container', {
+    slideClass: 'feedback__items',
+    wrapperClass: 'feedback__wrapper', 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
   
 
   var mixer = mixitup('.products__wrapper');
 
 });
 
-let mySwiper = new Swiper('.feedback__swiper-container', {
-  slideClass: 'feedback__items',
-  wrapperClass: 'feedback__wrapper',
-});
+
 
